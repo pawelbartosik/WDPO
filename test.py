aec74d4a242695f114f78e6836738bdeda238b9c
Pablo <paba.0018@gmail.com>
2022-12-09 17:23:09 +0100

    finished mask i hope xd

--- test.py
+++ test.py
@@ -42,18 +42,28 @@ import numpy as np
 #     cv2.imshow('hsv_img', resize_mask)
 
 # APPLY MASK
-img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\03.jpg"
-cv2.namedWindow('hsv')
+img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\00.jpg"
 img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 #TUTAJ MASKI
+mask_hsv_green = cv2.inRange(hsv, (33, 190, 0), (146, 255, 255))
+mask_hsv_purple = cv2.inRange(hsv, (138, 84, 0), (170, 255, 255))
+mask_hsv_yellow = cv2.inRange(hsv, (6, 203, 83), (30, 255, 255))
+mask_hsv_red = cv2.inRange(hsv, (170, 43, 125), (255, 255, 255))
 
+#mask_hsv = cv2.inRange(hsv, (Hmin, Smin, Vmin), (Hmax, Smax, Vmax))
 
-mask_hsv = cv2.inRange(hsv, (Hmin, Smin, Vmin), (Hmax, Smax, Vmax))
 resize_img = cv2.resize(img, (500, 500))
-resize_mask = cv2.resize(mask_hsv, (500, 500))
+resize_mask_green = cv2.resize(mask_hsv_green, (500, 500))
+resize_mask_purple = cv2.resize(mask_hsv_purple, (500, 500))
+resize_mask_yellow = cv2.resize(mask_hsv_yellow, (500, 500))
+resize_mask_red = cv2.resize(mask_hsv_red, (500, 500))
 
 cv2.imshow('img', resize_img)
-cv2.imshow('hsv_img', resize_mask)
+cv2.imshow('green', resize_mask_green)
+cv2.imshow('purple', resize_mask_purple)
+cv2.imshow('yellow', resize_mask_yellow)
+cv2.imshow('red', resize_mask_red)
+cv2.waitKey()
 
