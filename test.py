7e08ee4330fa900c15fabc18d6c8d53854e6814f
Pablo <paba.0018@gmail.com>
2022-12-09 13:38:59 +0100

    finish in worok

--- test.py
+++ test.py
@@ -6,35 +6,54 @@ import cv2
 from tqdm import tqdm
 import numpy as np
 
-def dummy(x):
-    pass
 
+# # HSV TRACKBAR
+# def dummy(x):
+#     pass
+#
+# img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\03.jpg"
+# cv2.namedWindow('hsv')
+# cv2.createTrackbar('Hmax', 'hsv', 0, 255, dummy)
+# cv2.createTrackbar('Hmin', 'hsv', 0, 255, dummy)
+# cv2.createTrackbar('Smax', 'hsv', 0, 255, dummy)
+# cv2.createTrackbar('Smin', 'hsv', 0, 255, dummy)
+# cv2.createTrackbar('Vmax', 'hsv', 0, 255, dummy)
+# cv2.createTrackbar('Vmin', 'hsv', 0, 255, dummy)
+# img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+#
+# while True:
+#     key_code = cv2.waitKey(10)
+#     if key_code == 27:
+#         break
+#
+#     Hmax = cv2.getTrackbarPos('Hmax', 'hsv')
+#     Hmin = cv2.getTrackbarPos('Hmin', 'hsv')
+#     Smax = cv2.getTrackbarPos('Smax', 'hsv')
+#     Smin = cv2.getTrackbarPos('Smin', 'hsv')
+#     Vmax = cv2.getTrackbarPos('Vmax', 'hsv')
+#     Vmin = cv2.getTrackbarPos('Vmin', 'hsv')
+#
+#     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#     mask_hsv = cv2.inRange(hsv, (Hmin, Smin, Vmin), (Hmax, Smax, Vmax))
+#     resize_img = cv2.resize(img, (500, 500))
+#     resize_mask = cv2.resize(mask_hsv, (500, 500))
+#
+#     cv2.imshow('img', resize_img)
+#     cv2.imshow('hsv_img', resize_mask)
+
+# APPLY MASK
 img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\03.jpg"
 cv2.namedWindow('hsv')
-cv2.createTrackbar('Hmax', 'hsv', 0, 255, dummy)
-cv2.createTrackbar('Hmin', 'hsv', 0, 255, dummy)
-cv2.createTrackbar('Smax', 'hsv', 0, 255, dummy)
-cv2.createTrackbar('Smin', 'hsv', 0, 255, dummy)
-cv2.createTrackbar('Vmax', 'hsv', 0, 255, dummy)
-cv2.createTrackbar('Vmin', 'hsv', 0, 255, dummy)
 img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-while True:
-    key_code = cv2.waitKey(10)
-    if key_code == 27:
-        break
+#TUTAJ MASKI
 
-    Hmax = cv2.getTrackbarPos('Hmax', 'hsv')
-    Hmin = cv2.getTrackbarPos('Hmin', 'hsv')
-    Smax = cv2.getTrackbarPos('Smax', 'hsv')
-    Smin = cv2.getTrackbarPos('Smin', 'hsv')
-    Vmax = cv2.getTrackbarPos('Vmax', 'hsv')
-    Vmin = cv2.getTrackbarPos('Vmin', 'hsv')
 
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask_hsv = cv2.inRange(hsv, (Hmin, Smin, Vmin), (Hmax, Smax, Vmax))
-    resize_img = cv2.resize(img, (500, 500))
-    resize_mask = cv2.resize(mask_hsv, (500, 500))
+mask_hsv = cv2.inRange(hsv, (Hmin, Smin, Vmin), (Hmax, Smax, Vmax))
+resize_img = cv2.resize(img, (500, 500))
+resize_mask = cv2.resize(mask_hsv, (500, 500))
 
-    cv2.imshow('img', resize_img)
-    cv2.imshow('hsv_img', resize_mask)
+cv2.imshow('img', resize_img)
+cv2.imshow('hsv_img', resize_mask)
+
