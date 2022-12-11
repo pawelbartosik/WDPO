4b5dc7c626bd7b0b80ce736415f48c0523878e9e
Pablo <paba.0018@gmail.com>
2022-12-11 20:06:07 +0100

    kernel

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ import numpy as np
 # def dummy(x):
 #     pass
 #
-# img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\03.jpg"
+# img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\05.jpg"
 # cv2.namedWindow('hsv')
 # cv2.createTrackbar('Hmax', 'hsv', 0, 255, dummy)
 # cv2.createTrackbar('Hmin', 'hsv', 0, 255, dummy)
@@ -52,7 +52,7 @@ import numpy as np
 # TROCHE CIEMNIEJSZY FIOLETOWY I CZEWRWONY, ZOBACZYC JAK Z ZMNIEJSZANIE I POTEM SPRAWDZANIE POLA
 
 # APPLY MASK
-img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\39.jpg"
+img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\00.jpg"
 img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -63,6 +63,13 @@ mask_hsv_yellow = cv2.inRange(hsv, (6, 203, 83), (30, 255, 255))
 mask_hsv_red = cv2.inRange(hsv, (170, 43, 125), (255, 255, 255))
 
 #mask_hsv = cv2.inRange(hsv, (Hmin, Smin, Vmin), (Hmax, Smax, Vmax))
+kernel = np.ones((7, 7), np.uint8)
+kernel1 = np.ones((5, 5), np.uint8)
+#erosion = cv2.erode(mask_hsv_purple, kernel, iterations=1)
+opening = cv2.morphologyEx(mask_hsv_green, cv2.MORPH_OPEN, kernel)
+closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel1)
+dilation = cv2.dilate(closing, kernel, iterations=1)
+resize_erosion = cv2.resize(closing, (500, 500))
 
 resize_img = cv2.resize(img, (500, 500))
 resize_mask_green = cv2.resize(mask_hsv_green, (500, 500))
@@ -72,8 +79,10 @@ resize_mask_red = cv2.resize(mask_hsv_red, (500, 500))
 
 cv2.imshow('img', resize_img)
 cv2.imshow('green', resize_mask_green)
-cv2.imshow('purple', resize_mask_purple)
-cv2.imshow('yellow', resize_mask_yellow)
-cv2.imshow('red', resize_mask_red)
+# cv2.imshow('purple', resize_mask_purple)
+# cv2.imshow('yellow', resize_mask_yellow)
+# cv2.imshow('red', resize_mask_red)
+
+cv2.imshow('erosion', resize_erosion)
 cv2.waitKey()
 
