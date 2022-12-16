2d9c96c5992526ef6c9f05d2819c2a3a77e7e6b1
Pablo <paba.0018@gmail.com>
2022-12-16 12:34:01 +0100

    better

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ import numpy as np
 def dummy(x):
     pass
 
-img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\17.jpg"
+img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\12.jpg"
 cv2.namedWindow('hsv')
 cv2.createTrackbar('Hmax', 'hsv', 0, 255, dummy)
 cv2.createTrackbar('Hmin', 'hsv', 0, 255, dummy)
@@ -52,27 +52,27 @@ while True:
 # TROCHE CIEMNIEJSZY FIOLETOWY I CZEWRWONY, ZOBACZYC JAK Z ZMNIEJSZANIE I POTEM SPRAWDZANIE POLA
 
 # # APPLY MASK
-# img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\39.jpg"
+# img_path = "D:\\Studia\\Semestr 5\\WDPO Lab\\projekt\\WDPO\\data\\12.jpg"
 # img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 # blur_bilateral = cv2.bilateralFilter(img, 15, 75, 75)
 # hsv = cv2.cvtColor(blur_bilateral, cv2.COLOR_BGR2HSV)
 # # blur_median = cv2.medianBlur(img, 10)
 # # cv2.imshow('blur', blur_bilateral)
 # #TUTAJ MASKI
-# mask_hsv_green = cv2.inRange(hsv, (33, 190, 0), (146, 255, 255))
-# mask_hsv_purple = cv2.inRange(hsv, (138, 84, 0), (170, 255, 255))
+# mask_hsv_green = cv2.inRange(hsv, (33, 190, 0), (146, 255, 200))
+# mask_hsv_purple = cv2.inRange(hsv, (138, 49, 38), (170, 255, 255))
 # mask_hsv_yellow = cv2.inRange(hsv, (6, 203, 83), (30, 255, 255))
 # mask_hsv_red = cv2.inRange(hsv, (170, 43, 125), (255, 255, 255))
 #
 # #mask_hsv = cv2.inRange(hsv, (Hmin, Smin, Vmin), (Hmax, Smax, Vmax))
-# kernel = np.ones((10, 10), np.uint8)
-# kernel1 = np.ones((9, 9), np.uint8) #11, 11 green,
-# kernel2 = np.ones((11, 11), np.uint8)#5, 5 green,
+# kernel = np.ones((5, 5), np.uint8)
+# kernel1 = np.ones((11, 11), np.uint8) #11, 11 green,
+# kernel2 = np.ones((5, 5), np.uint8)#5, 5 green,
 # #erosion = cv2.erode(mask_hsv_purple, kernel, iterations=1)
-# opening = cv2.morphologyEx(mask_hsv_green, cv2.MORPH_OPEN, kernel)
+# opening = cv2.morphologyEx(mask_hsv_purple, cv2.MORPH_OPEN, kernel)
 # closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel1)
-# erosion = cv2.erode(closing, kernel1, iterations=1)
-# dilation = cv2.dilate(erosion, kernel2, iterations=1)
+# erosion = cv2.erode(closing, kernel2, iterations=1)
+# dilation = cv2.dilate(erosion, kernel, iterations=1)
 # resize_closing = cv2.resize(closing, (500, 500))
 # resize_opening = cv2.resize(opening, (500, 500))
 # dilation_res = cv2.resize(dilation, (500, 500))
@@ -88,7 +88,15 @@ while True:
 # #     print(i[0])
 #
 # cnt, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# print(len(cnt))
+# # cv2.contourArea(cnt[0])
+# amount = 0
+# for c in cnt:
+#     # print(c)
+#     size = cv2.contourArea(c)
+#     print(size)
+#     if size > 3000:
+#         amount = amount + 1
+# print(amount)
 #
 # resize_img = cv2.resize(img, (500, 500))
 # resize_mask_green = cv2.resize(mask_hsv_green, (500, 500))
@@ -97,8 +105,8 @@ while True:
 # resize_mask_red = cv2.resize(mask_hsv_red, (500, 500))
 #
 # cv2.imshow('img', resize_img)
-# cv2.imshow('green', resize_mask_green)
-# # cv2.imshow('purple', resize_mask_purple)
+# # cv2.imshow('green', resize_mask_green)
+# cv2.imshow('purple', resize_mask_purple)
 # # cv2.imshow('yellow', resize_mask_yellow)
 # # cv2.imshow('red', resize_mask_red)
 # #
--- tester.py
+++ tester.py
@@ -32,7 +32,7 @@ def detect(img_path: str) -> Dict[str, int]:
     hsv = cv2.cvtColor(blur_bilateral, cv2.COLOR_BGR2HSV)
 
     # green
-    mask_hsv_green = cv2.inRange(hsv, (33, 190, 0), (146, 255, 255))
+    mask_hsv_green = cv2.inRange(hsv, (33, 190, 0), (55, 255, 255))
     kernel = np.ones((7, 7), np.uint8)
     kernel1 = np.ones((11, 11), np.uint8)
     kernel2 = np.ones((5, 5), np.uint8)
@@ -41,10 +41,13 @@ def detect(img_path: str) -> Dict[str, int]:
     erosion = cv2.erode(closing, kernel1, iterations=1)
     dilation = cv2.dilate(erosion, kernel2, iterations=1)
     cnt, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    green = len(cnt)
+    for c in cnt:
+        size = cv2.contourArea(c)
+        if size > 3000:
+            green = green + 1
     # purple
-    mask_hsv_purple = cv2.inRange(hsv, (138, 84, 0), (170, 255, 255))
-    kernel = np.ones((14, 14), np.uint8)
+    mask_hsv_purple = cv2.inRange(hsv, (145, 24, 10), (177, 255, 122))
+    kernel = np.ones((5, 5), np.uint8)
     kernel1 = np.ones((11, 11), np.uint8)
     kernel2 = np.ones((5, 5), np.uint8)
     opening = cv2.morphologyEx(mask_hsv_purple, cv2.MORPH_OPEN, kernel)
@@ -52,7 +55,10 @@ def detect(img_path: str) -> Dict[str, int]:
     erosion = cv2.erode(closing, kernel2, iterations=1)
     dilation = cv2.dilate(erosion, kernel, iterations=1)
     cnt, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    purple = len(cnt)
+    for c in cnt:
+        size = cv2.contourArea(c)
+        if size > 3000:
+            purple = purple + 1
     # yellow
     mask_hsv_yellow = cv2.inRange(hsv, (6, 203, 83), (30, 255, 255))
     kernel = np.ones((7, 7), np.uint8)
@@ -63,7 +69,10 @@ def detect(img_path: str) -> Dict[str, int]:
     erosion = cv2.erode(closing, kernel1, iterations=1)
     dilation = cv2.dilate(erosion, kernel2, iterations=1)
     cnt, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    yellow = len(cnt)
+    for c in cnt:
+        size = cv2.contourArea(c)
+        if size > 3000:
+            yellow = yellow + 1
     # red
     mask_hsv_red = cv2.inRange(hsv, (170, 43, 125), (255, 255, 255))
     kernel = np.ones((10, 10), np.uint8)
@@ -74,7 +83,10 @@ def detect(img_path: str) -> Dict[str, int]:
     erosion = cv2.erode(closing, kernel1, iterations=1)
     dilation = cv2.dilate(erosion, kernel2, iterations=1)
     cnt, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    red = len(cnt)
+    for c in cnt:
+        size = cv2.contourArea(c)
+        if size > 3000:
+            red = red + 1
 
     return {'red': red, 'yellow': yellow, 'green': green, 'purple': purple}
 
@@ -103,25 +115,36 @@ def main(data_path: Path, output_file_path: Path):
         expected = json.load(f)
 
     notwork = []
+    notwork_green = []
+    notwork_purple = []
+    notwork_yellow = []
+    notwork_red = []
 
     for image in results:
         if results[image]['red'] != expected[image]['red']:
             print(image, "red")
+            notwork_red.append(image)
             notwork.append(image)
         if results[image]['green'] != expected[image]['green']:
             print(image, "green")
+            notwork_green.append(image)
             notwork.append(image)
         if results[image]['purple'] != expected[image]['purple']:
             print(image, "purple")
+            notwork_purple.append(image)
             notwork.append(image)
         if results[image]['yellow'] != expected[image]['yellow']:
             print(image, "yellow")
+            notwork_yellow.append(image)
             notwork.append(image)
     notwork1 = []
     [notwork1.append(x) for x in notwork if x not in notwork1]
     print(notwork1)
-    print(len(notwork1))
-
+    print('Total:', len(notwork1))
+    print('Green:', len(notwork_green))
+    print('purple:', len(notwork_purple))
+    print('red:', len(notwork_red))
+    print('yellow:', len(notwork_yellow))
 
 if __name__ == '__main__':
     main()
